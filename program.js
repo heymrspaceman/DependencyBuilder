f631b92cc54b7009f9a90986526a5508a0fda666
toadordragon <heymrspaceman@users.noreply.github.com>
2015-05-13 22:39:31 +0100

    Started to add file handling code

--- program.js
+++ program.js
@@ -1,5 +1,7 @@
 /// <reference path="typings/node/node.d.ts"/>
 
+var fs = require('fs');
 // For each project we will read a components CSV file
+// Read the file
 
 // For component we will read a references scv file
